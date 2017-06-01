9734cd97e268333013b4f90078cdca52b2459e9c
ssthil <ssthil@gmail.com>
2017-06-02 00:11:52 +0800

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,25 @@ export class AppComponent {
   title = 'app works!';
 
   clips = {};
-  endTimeMinTotal = [];
-  endTimeSecTotal = [];
+
   totalMinTime: number= 0;
   totalSecTime: number= 0;
   totalTime:number = 0;
 
   //test
-  clipsDefinition:any[] = [];
-  clipsStandard:any[] = [];
+  clipsDefinition:any[];
+  clipsStandard:any[];
   clipsEndTimeArr:any[];
+
+  //HD SD PAL NTSC
+  clipsPALDefinitionSD:any[];
+  clipsPALDefinitionHD:any[];
+  clipsNTSCDefinitionSD:any[];
+  clipsNTSCDefinitionHD:any[];
+
+  //reel
+  reelHD:any[];
+  reelSD:any[];
 
   //time
   hh:any[]; mm:any[]; ss:any[]; ff:any[]; totalHH:number; totalMM:number; totalSS:number; totalFF:number;
@@ -28,30 +37,23 @@ export class AppComponent {
 
   ngOnInit() {
     
-    this.getVideoClips();
     this.getClipsDefinition();
     this.getClipsStandard();
+
+    this.createStandardClips("PAL", "SD");
     
-    this.getSameStandardClips("PAL", "SD");
-    this.getSameStandardClips("NTSC", "SD");
+    this.getStandardClipsTime("PAL", "SD");
+    this.getStandardClipsTime("NTSC", "SD");
     
   }
 
-  //video clips 
-  getVideoClips() {
-    this._clipsService.readAll()
-      .subscribe((res) => {
-        this.clips = res; 
-        //console.log(this.clips); 
-      });
-      
-  }
 
   //find video clips Definition 
   getClipsDefinition(){
     this._clipsService.readAll()
       .subscribe((data) => {
         this.clips = data;
+        this.clipsDefinition = [];
 
         for (var val in this.clips) {
             var obj = this.clips[val];
@@ -68,6 +70,7 @@ export class AppComponent {
     this._clipsService.readAll()
       .subscribe((data) => {
         this.clips = data;
+        this.clipsStandard = [];
 
         for(var val in this.clips) {
           var obj = this.clips[val];
@@ -79,9 +82,48 @@ export class AppComponent {
       })
   }
 
+  //add clip
+  addReel() {
+    this._clipsService.readAll()
+      .subscribe((data) => {
+        this.clips = data;
+        
+
+      })
+  }
+
+  //I navigate to the user interface and create a PAL SD Video Reel
+  createStandardClips(clipStandard:string, clipDefinition:string) {
+    this._clipsService.readAll()
+      .subscribe((data) => {
+        this.clips = data;
+        this.clipsPALDefinitionSD = [];
+        this.clipsPALDefinitionHD = [];
+        this.clipsNTSCDefinitionSD = [];
+        this.clipsNTSCDefinitionHD = [];
+
+        for(let val in this.clips) {
+          let obj = this.clips[val];
+
+          if(obj.Standard === clipStandard) {
+            (obj.Definition === clipDefinition) ? this.clipsPALDefinitionSD.push(obj) : this.clipsPALDefinitionHD.push(obj);        
+          }else {
+            (obj.Definition === clipDefinition) ? this.clipsNTSCDefinitionSD.push(obj): this.clipsNTSCDefinitionHD.push(obj);
+          }
+
+        }
+        console.log(this.clipsPALDefinitionSD);
+        console.log(this.clipsPALDefinitionHD);
+        console.log(this.clipsNTSCDefinitionSD);
+        console.log(this.clipsNTSCDefinitionHD);
+        
+        
+      })
+  }
+
   //total duration displayed is 00:02:11:01 when I add all the PAL SD video clips
   //total duration displayed is 00:00:54:08 when I add all the NTSC SD video clips
-  getSameStandardClips(clipStandard:string, clipDefinition:string) { 
+  getStandardClipsTime(clipStandard:string, clipDefinition:string) { 
     
      this._clipsService.readAll()
       .subscribe((data) => {
@@ -97,22 +139,22 @@ export class AppComponent {
         this.totalSS = 0;
         this.totalFF = 0;
 
-        for (var val in this.clips) { 
-            var obj = this.clips[val];
+        for (let val in this.clips) { 
+            let obj = this.clips[val];
             
            if(obj.Standard === clipStandard && obj.Definition === clipDefinition) {  
               this.clipsEndTimeArr.push(obj.End);
            } 
            
         }
-        for(var i=0; i<this.clipsEndTimeArr.length; i++) {
-          var clipsSplitEndTimeArr = this.clipsEndTimeArr[i].split(":");
+        for(let i=0; i<this.clipsEndTimeArr.length; i++) {
+          let clipsSplitEndTimeArr = this.clipsEndTimeArr[i].split(":");
           this.hh.push(clipsSplitEndTimeArr[0]);
           this.mm.push(clipsSplitEndTimeArr[1]);
           this.ss.push(clipsSplitEndTimeArr[2]);
           this.ff.push(clipsSplitEndTimeArr[3]);
         }
-        for(var i=0; i<this.ss.length; i++) { 
+        for(let i=0; i<this.ss.length; i++) { 
           this.totalHH +=parseFloat(this.hh[i]);
           this.totalMM +=parseFloat(this.mm[i]);
           this.totalSS +=parseFloat(this.ss[i]);
